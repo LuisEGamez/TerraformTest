c0580d89d687f0951c545b663d4774bec73d72c6
LuisEGamez <luisgamezdvp@gmail.com>
2024-06-20 18:14:23 +0200

    add project test first commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "iam" {
+  source = "./modules/iam"
+}
+
+module "ec2" {
+  source                = "./modules/ec2"
+  vpc_id                = module.vpc.vpc_id
+  instance_profile_name = module.iam.instance_profile_name
+  public_subnet_ip      = module.vpc.public_subnet_id
+  private_subnets_ids   = module.vpc.private_subnets_ids
+  elb_security_group_id = module.elb.elb_security_group_id
+}
+
+module "elb" {
+  source      = "./modules/elb"
+  vpc_id      = module.vpc.vpc_id
+  instance_id = module.ec2.ec2_id
+  private_subnets_ids = module.vpc.private_subnets_ids
+}
+
---
+++ output.tf
@@ -0,0 +1,16 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "sg_id" {
+  value = module.ec2.sg_id
+}
+
+output "ec2_id" {
+  value = module.ec2.ec2_id
+}
+
+output "elb_dns_name" {
+  value = module.elb.elb_dns_name
+}
+
